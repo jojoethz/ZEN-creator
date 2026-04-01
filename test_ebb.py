e63241a6412fb2d0086a19d7a75edeadfe82474a
Joël Lauper <jlauper@ethz.ch>
2026-04-01 12:46:10 +0200

    prepare test

--- test_ebb.py
+++ test_ebb.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 from zen_creator.model import Model
 
-existing_model_path = Path("./path/to/existing_model")
-model = Model.from_existing(existing_model_path=existing_model_path)
+# 1) Load existing model
+model = Model.from_existing(Path(r"C:\Users\joell\Documents\ETH\Master\master_thesis\ZEN-garden\Europe_but_better_joel"))
+
+# 2) Modify model
+model.output_folder = Path(r"C:\Users\joell\Documents\ETH\Master\master_thesis\ZEN-garden")
+model.name = "Ebb_joel_with_custom_changes"
+
+# 3) Rebuild to apply subclass-specific _set_ logic
+model.build()
+
+# 4) Validate and write files
+model.write()
