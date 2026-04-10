0a6ed70219f20f29f14a0f6878a5b844f70ad2b6
Joël Lauper <jlauper@ethz.ch>
2026-04-10 17:26:12 +0200

    christoph for loop to adapt model

--- test_ebb.py
+++ test_ebb.py
@@ -9,11 +9,15 @@ model = Model.from_existing(Path("C:/Users/joell/Documents/ETH/Master/master_the
 model.output_folder = Path("C:/Users/joell/Documents/ETH/Master/master_thesis/ZEN-garden")
 model.name = "Ebb_joel_with_custom_changes"
 
+technology_list = ["solar", "wind", "pumped_hydro"]
+for technology in model.technologies:
+    if technology.name in technology_list:
+        capacity_addition_min = TYNDP2024(model.source_path).get_capacity_addition_min(technology.name)
 
+        technology.capacity_addition_min.set_data(df=capacity_addition_min)
 
-
-# 3) Rebuild to apply subclass-specific _set_ logic
-#model.build()
+# # 3) Rebuild to apply subclass-specific _set_ logic, use only later
+# model.build()
 
 # 4) Validate and write files
 model.write()
