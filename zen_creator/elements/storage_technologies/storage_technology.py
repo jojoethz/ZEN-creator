21d0bf4d618ab082ec851c39b0b10720a0adf331
Joël Lauper <jlauper@ethz.ch>
2026-05-07 10:00:28 +0200

    storage fix: set default value = 0 for _capacity_lower_limit_energy

--- zen_creator/elements/storage_technologies/storage_technology.py
+++ zen_creator/elements/storage_technologies/storage_technology.py
@@ -98,7 +98,7 @@ class StorageTechnology(Technology, ABC):
 
         self._capacity_lower_limit_energy = Attribute(
             "capacity_lower_limit_energy",
-            default_value=np.nan,  # Use NaN again so it only enforces when given data
+            default_value=0.0,
             unit=f"{self.power_unit}*h",
             element=self,
         )
